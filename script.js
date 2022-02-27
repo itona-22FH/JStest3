98f977474ff1a1df22c3a3bd8b08d58ae7316dc7
itona-22FH <naov7700@gmail.com>
2022-02-27 11:02:38 +0900

    JavaScript課題3-1 修正

--- script.js
+++ script.js
@@ -4,36 +4,40 @@ const taskList = [];
 const taskAddBtn = document.getElementById('taskAddBtn');
 const todoList = document.getElementById('todoList');
 
+const createStatusBtn = value => {
+  const statusTd = document.createElement('td');
+  const statusBtn = document.createElement('button');
+  statusBtn.textContent = value.status;
+  statusTd.appendChild(statusBtn);
+  return statusTd;
+};
+
+const createDeleteBtn = () => {
+  const deleteTd = document.createElement('td');
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = '削除';
+  deleteTd.appendChild(deleteBtn);
+  return deleteTd;
+};
+
 taskAddBtn.addEventListener('click', function () {
   if (inputTask.value) {
     todoList.innerHTML = '';
     const inputTask = document.getElementById('inputTask');
-    taskList.push({ task: inputTask.value});
+    taskList.push({ task: inputTask.value, status: '作業中' });
 
     taskList.forEach((value, i) => {
       const newTr = document.createElement('tr');
-
       const idTd = document.createElement('td');
       const taskTd = document.createElement('td');
-      const statusTd = document.createElement('td');
-      const deleteTd = document.createElement('td');
-
-      const statusBtn = document.createElement('button');
-      statusBtn.textContent = '作業中';
-      statusTd.appendChild(statusBtn);
-      const deleteBtn = document.createElement('button');
-      deleteBtn.textContent = '削除';
-      deleteTd.appendChild(deleteBtn);
 
       idTd.textContent = i;
       taskTd.textContent = value.task;
 
-      statusTd.appendChild(statusBtn);
-
       newTr.appendChild(idTd);
       newTr.appendChild(taskTd);
-      newTr.appendChild(statusTd);
-      newTr.appendChild(deleteTd);
+      newTr.appendChild(createStatusBtn(value));
+      newTr.appendChild(createDeleteBtn());
 
       todoList.appendChild(newTr);
     });
